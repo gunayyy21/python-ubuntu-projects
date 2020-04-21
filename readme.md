d72a7e84b4e55bdef59f3b8af8569174100c9118
gunayyy21 <gunaykaraoglu21@gmail.com>
2020-04-21 20:06:56 +0300

    changes are made

---
+++ readme.md
@@ -0,0 +1,2 @@
+this repository is for education purposes only while I'm excercising over git usage via terminal in a
+ ubuntu machine
---
+++ untitled/main.py
@@ -0,0 +1,8 @@
+'''
+    This project is for education purposes only .Check readme file for more information
+
+'''
+
+a = int(input("tell me your age in years : "));
+
+print("you are at least ",a*365,"days old")
